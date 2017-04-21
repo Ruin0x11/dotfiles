645f03a726e86ca85de1bf2224c1d5a876397094
Ian Pickering <ipickering2@gmail.com>
2017-04-21 09:09:14 -0500

    zsh submodules

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="config bashrc vimrc vimrc.bundles zshrc oh-my-zsh zsh Xresources Xcolors xinitrc muttrc mpdscribble mutt mpd aliases ncmpcpp bin vimperatorrc tmux.conf gitconfig wifi xmodmap"    # list of files/folders to symlink in homedir
+files="config bashrc vimrc vimrc.bundles zshrc zsh Xresources Xcolors xinitrc muttrc mpdscribble mutt mpd aliases ncmpcpp bin tmux.conf gitconfig wifi xmodmap fvwmrc"    # list of files/folders to symlink in homedir
 
 ##########
 
